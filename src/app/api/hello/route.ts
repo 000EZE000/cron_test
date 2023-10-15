8dd42294b3f0667aea7987ab252596deefe278fa
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-10-15 01:04:44 -0300

    <PRISMA>>>

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,48 +1,23 @@
-import { IFormMail } from "@/config/type";
-import nodemailer from "nodemailer"
 import "dotenv/config"
+import { PrismaClient } from "@prisma/client";
+import { randomUUID } from "crypto";
 
 
 export async function GET(request: any) {
+  const prismaInit = new PrismaClient()
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.API_KEY_EMAIL,
-      },
-    });
-
-    const getFormMail = (data: IFormMail): IFormMail => {
-      return {
-        from: data.from,
-        to: data.to,
-        subject: data.subject,
-        html: data.html,
-      };
-    };
-
-    const promiseApiNodemailer = (prop: IFormMail) =>
-      new Promise((resolve, reject) => {
-        transporter.sendMail(prop, (error, info) => {
-          if (error) {
-            console.log({ data: "data" });
-            return reject(error);
-          }
-          resolve(info.response);
-        });
-      });
-    const data = getFormMail({
-      from: "ezequiel.ignacio.sosa@gmail.com",
-      to: "ezequiel.ignacio.sosa@gmail.com",
-      subject: "cron",
-      html: "<h1>VAMOSsS </h1>",
-    });
-    const result = await promiseApiNodemailer(data);
-    return new Response(JSON.stringify({ data, result }), {
+    const result = await prismaInit.user.create({
+      data: {
+        email: "ezequiel" + randomUUID(),
+        name: "ezequiel"
+      }
+    })
+    await prismaInit.$disconnect()
+    return new Response(JSON.stringify({ result }), {
       status: 200,
     });
   } catch (error: any) {
+    await prismaInit.$disconnect()
     return new Response(JSON.stringify({ data: "sadasd" }), {
       status: 200,
     });
