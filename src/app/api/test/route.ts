a6a2d30917b362d39faaf859d797d2118554b286
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-10-14 21:47:31 -0300

    <FIX>

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export async function GET(request: any) {
-  console.log({ dad: "adsadasdsadsadsada" });
   return new Response("dasdasdasdsad", { status: 200 });
 }
