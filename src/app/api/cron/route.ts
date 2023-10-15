611b120e85bec68030d5643d77a8b2976e78cad4
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-10-15 00:15:01 -0300

    <FIX> update

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,9 +1,39 @@
 import { getFormMail, promiseApiNodemailer } from "@/config";
+import { IFormMail } from "@/config/type";
+import nodemailer from "nodemailer"
+
 
 export async function GET(request: any) {
   try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "ezequiel.ignacio.sosa@gmail.com",
+        pass: "unyohrilzfebybod",
+      },
+    });
+
+    const getFormMail = (data: IFormMail): IFormMail => {
+      return {
+        from: data.from,
+        to: data.to,
+        subject: data.subject,
+        html: data.html,
+      };
+    };
+
+    const promiseApiNodemailer = (prop: IFormMail) =>
+      new Promise((resolve, reject) => {
+        transporter.sendMail(prop, (error, info) => {
+          if (error) {
+            console.log({ data: "data" });
+            return reject(error);
+          }
+          resolve(info.response);
+        });
+      });
     const data = getFormMail({
-      from: "eds",
+      from: "ezequiel.ignacio.sosa@gmail.com",
       to: "ezequiel.ignacio.sosa@gmail.com",
       subject: "ca",
       html: "<h1>VAMOSsS </h1>",
