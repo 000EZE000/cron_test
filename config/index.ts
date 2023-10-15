427ff1a6e0f168f9b0040c8887734d74ecfccea4
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-10-14 23:57:43 -0300

    <FIX> update

--- config/index.ts
+++ config/index.ts
@@ -3,6 +3,9 @@ import { IFormMail } from "./type";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.sendgrid.net",
+  port: 465,
+  secure: true,
   auth: {
     user: "ezequiel.ignacio.sosa@gmail.com",
     pass: "unyohrilzfebybod",
@@ -22,7 +25,7 @@ export const promiseApiNodemailer = (prop: IFormMail) =>
   new Promise((resolve, reject) => {
     transporter.sendMail(prop, (error, info) => {
       if (error) {
-        console.log({ data: "data" })
+        console.log({ data: "data" });
         return reject(error);
       }
       resolve(info.response);
