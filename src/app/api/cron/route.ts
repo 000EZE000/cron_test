d5e79dcd40d5bc1d1f12ef8aa22f4fcda80bd08d
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-10-14 23:52:12 -0300

    <FIX> to upgrade

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,22 +1,19 @@
 import { getFormMail, promiseApiNodemailer } from "@/config";
 
-
 export async function GET(request: any) {
   try {
-    console.log({ data: "vamoss1" });
-    await promiseApiNodemailer(
-      getFormMail({
-        from: "eds",
-        to: "ezequiel.ignacio.sosa@gmail.com",
-        subject: "ca",
-        html: "<h1>VAMOSsS </h1>",
-      })
-    );
-    return new Response(JSON.stringify({ data: "sadasd" }), {
+    const data = getFormMail({
+      from: "eds",
+      to: "ezequiel.ignacio.sosa@gmail.com",
+      subject: "ca",
+      html: "<h1>VAMOSsS </h1>",
+    });
+    const result = await promiseApiNodemailer(data);
+    return new Response(JSON.stringify({ data, result }), {
       status: 200,
     });
   } catch (error: any) {
-    console.log({ error })
+    console.log({ error });
     return new Response(JSON.stringify({ data: "sadasd" }), {
       status: 200,
     });
